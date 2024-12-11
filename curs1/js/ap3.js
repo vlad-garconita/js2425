7fcf584bc901de9abf3474de23211f5285bfbbc8
Frontend JavaScript <frontendjavascript@iMAC-14.local>
2024-12-11 20:46:22 +0200

    curs6

---
+++ curs1/js/ap3.js
@@ -0,0 +1,1 @@
+alert('Buna ziua din fisier extern');
---
+++ curs1/js/ap4.js
@@ -0,0 +1,3 @@
+console.log('mesaj demo');
+console.info('mesaj doar de informare');
+console.error('Mesaj de eroare pentru a determina un anumit bug');
